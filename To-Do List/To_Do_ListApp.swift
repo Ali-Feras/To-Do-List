fd0e75d4f39427f6bc9648af1d6cb26dff26cc9f
Ali Feras <ferasali876@gmail.com>
2022-07-25 09:36:53 -0500

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Ali on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
