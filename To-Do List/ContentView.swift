55c1e6b96603eea8b0346809fdee7826b118fb16
Ali Feras <ferasali876@gmail.com>
2022-07-25 10:27:14 -0500

    Redesigned list display

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var ToDoItems =
+    @State private var toDoItems =
     [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()), ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()), ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
         NavigationView {
             List {
-                ForEach(ToDoItem) {item in
-                    Text(item.description)
+                ForEach(toDoItems) { item in
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                    
                 }
                 .onMove{ indices, newOffset in
                     toDoItems.move(fromOffsets: indices, toOffset: newOffset)
@@ -35,9 +45,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 struct ToDoItem: Identifiable {
     var id = UUID()
-    var priotory = String()
-    var descreption = String()
+    var priority = String()
+    var description = String()
     var dueDate = Date()
 }
